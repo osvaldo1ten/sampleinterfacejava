618d917207188c83b3c5c76bb1a77dd2ba5bf87c
osvaldo1ten <dgposvaldo@gmail.com>
2021-04-11 00:18:50 -0300

    classe contendo o metodo main da aplicacao

---
+++ AppExInteface.java
@@ -0,0 +1,11 @@
+public class AppExInteface {
+
+	public static void main(String[] args) {
+		
+		Ferrari ferrari = new Ferrari();
+		
+		Route route = new Route();
+		
+		route.andar(ferrari);
+	}
+}
